6c597e26f6af6d1920cd2669215e2f0f1dae27c1
Richarlison Lisboa <richarlison.lisboa@gmail.com>
2022-06-13 23:19:47 -0300

    Add files via upload

---
+++ totem/resources/lang/en/messages.php
@@ -0,0 +1,180 @@
+<?php
+
+return [
+    'save' => 'Salvar',
+    'select' => 'Selecionar',
+    'add' => 'Adicionar',
+    'name' => 'Nome',
+    'actions' => 'Ações',
+    'edit' => 'Editar',
+    'delete' => 'Excluir',
+    'update' => 'Atualizar',
+    'change' => 'Alterar',
+    'date' => 'Encontro',
+    'go' => 'Ir',
+    'range' => 'Alcance',
+    'report' => 'Relatório',
+    'month' => 'Mês',
+    'all' => 'Todos',
+    'starting' => 'Iniciando',
+    'ending' => 'Final',
+    'time' => 'Tempo',
+    'issue' => 'Painel',
+    'new' => 'Novo',
+    'settings' => 'Configurações',
+    'default' => 'Padrão',
+    'set' => 'Conjunto',
+    'language' => 'Língua',
+    'next_only' => 'Próximo',
+    'cancel' => 'Cancelar',
+    'type' => 'Digitar',
+    'types' => 'Tipos',
+    'serving' => 'Servindo',
+    'served' => 'Servido',
+    'start' => 'Começar',
+    'end' => 'Fim',
+    'and' => 'E',
+    'in_seconds' => 'Em segundos',
+    'avg_time' => 'Tempo Médio de Espera',
+    'min_time' => 'Tempo mínimo de espera',
+    'max_time' => 'Tempo máximo de espera',
+    'count_perday' => 'Contagem total por dia',
+    'font_size' => 'Tamanho da fonte',
+    'color' => 'Cor',
+    'today_queue' => 'Fila de hoje',
+    'today_missed' => 'Hoje falta Atender',
+    'today_served' => 'Hoje Foram atendidos',
+    'over_time' => 'Ao longo do tempo',
+    'today_yesterday' => 'Hoje vs Ontem',
+    'queue_details' => 'Detalhes da Fila',
+    'more_info' => 'Mais informações',
+
+    'locales' => [
+        'pt' => [
+            'gb' => 'English',
+            'tr' => 'Türk',
+            'sa' => 'العربية',
+            'fr' => 'français',
+            'de' => 'Deutsch',
+            'in' => 'हिन्दी',
+            'it' => 'italiano',
+            'pt' => 'português',
+            'ru' => 'русский',
+            'sk' => 'slovenský',
+            'es' => 'Español',
+            'th' => 'ไทย',
+            'id' => 'Indonesia',
+        ],
+    ],
+
+    'mainapp' => [
+        'select_language' => 'Idioma...',
+        'display_url' => 'Mostrar o URL',
+        'issue_url' => 'URL de Emissão de senhas',
+        'last_login' => 'Último login',
+        'profile' => 'Perfil',
+        'logout' => 'Sair',
+        'menu' => [
+            'menu' => 'Cardápio',
+            'dashboard' => 'Painel de Controle',
+            'call' => 'Chamada de Senhas',
+            'department' => 'Tipo  de Serviço',
+            'counter' => 'Guichê',
+            'users' => 'Operadores',
+            'reports' => [
+                'reports' => 'Relatórios',
+                'user_report' => 'Denunciar operador',
+                'queue_list' => 'Lista de fila',
+                'monthly' => 'Por mês',
+                'statistical' => 'Estatística',
+                'missed' => 'Esquecidas',
+                'overtime' => 'Ao longo do tempo',
+            ],
+            'account' => 'Conta',
+        ],
+
+        'role' => [
+            'Administrator' => 'Administrador',
+            'Staff' => 'Funcionários'
+        ],
+    ],
+
+    'dashboard' => [
+        'notification' => 'Notificação',
+        'preview' => 'visualização',
+        'notification_text' => 'texto de notificação',
+        'notification_placeholder' => 'Notificação para mostrar na página de visualização',
+    ],
+
+    'call' => [
+        'new_call' => 'Nova chamada',
+        'user' => 'Operador',
+        'call_next' => 'Chamar Próxima Senha',
+        'click_department' => 'Selecione uma opção para emitir a senha de atendimento:',
+        'todays_queue' => 'Fila de hoje',
+        'number' => 'Número',
+        'called' => 'Chamado',
+        'recall' => 'recordação',
+    ],
+
+    'department' => [
+        'letter' => 'Carta',
+        'start' => 'Começar',
+    ],
+
+    'users' => [
+        'username' => 'Nome de operador',
+        'email' => 'O email',
+        'role' => 'Função',
+        'password' => 'Senha',
+        'confirm' => 'Confirmar',
+    ],
+
+    'company' => [
+        'company' => 'companhia',
+        'address' => 'Endereço',
+        'location' => 'Localização',
+        'phone' => 'Telefone',
+    ],
+
+    'display' => [
+        'display' => 'Exibição',
+        'token' => 'Senha',
+        'please' => 'Por favor',
+        'proceed_to' => 'Dirija-se ao:',
+    ],
+
+
+
+    // Dont want to change
+
+    'title' => 'Laravel Installer',
+    'next' => 'Next Step',
+    'finish' => 'Install',
+
+    'welcome' => [
+        'title'   => 'Bem vindo ao instalador',
+        'message' => 'Bem vindo ao instalador.',
+    ],
+
+    'requirements' => [
+        'title' => 'Requerimentos',
+    ],
+
+    'permissions' => [
+        'title' => 'Permissões',
+    ],
+
+    'environment' => [
+        'title' => 'Environment Settings',
+        'save' => 'Salvar .env',
+        'success' => 'Suas configuraçãos foram salvas no arquivo .env.',
+        'errors' => 'Não foi possível criar o arquivo .env por favor crie o manualmente.',
+    ],
+
+    'final' => [
+        'title' => 'Finalizado',
+        'finished' => 'O aplicativo foi instalado com sucesso!.',
+        'exit' => 'Clique aqui para finalizar.',
+    ],
+];
